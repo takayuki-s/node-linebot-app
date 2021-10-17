e97af730e1a7ae2670348c04ceaf78c57cb3acbd
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2021-10-17 17:29:47 +0900

    ミドルウェアの設定

--- index.js
+++ index.js
@@ -4,3 +4,10 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 const TOKEN = process.env.LINE_ACCESS_TOKEN;
+
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
