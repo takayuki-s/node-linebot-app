33bab4191fd010a0d93e4356bbffbc50dc8fdf04
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2021-10-26 23:18:46 +0900

    初期化

---
+++ server.js
@@ -0,0 +1,7 @@
+const http = require("http");
+
+const server = http.createServer((request, response) => {
+  response.writeHead(200, { "Content-Type": "text/html" });
+  response.end("Hello World!");
+});
+server.listen(8080);
